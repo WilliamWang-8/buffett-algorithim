5aa5f36d43a02f63e26722175a5eaacbf90db52f
William Wang <williamblue543@gmail.com>
2025-03-31 15:14:40 -0400

    selling logic

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import finnhub
 import alpaca_trade_api as alpaca
 import random
 import requests
+import sys
 
 # set up market api client
 market_key = "cvkbmnpr01qu5brnqs70cvkbmnpr01qu5brnqs7g"
@@ -24,8 +25,8 @@ for index in etf_index:
     dps.append(current_index["dp"])
 
 # determine average dp for overall market trend
-average_dp = 0.03
-print("Current Market dp: ", average_dp)
+average_dp = sum(dps)/3
+print("Current Market dp:", average_dp,"%")
 
 # assign investment amount and buy/sell based off market trend
 if average_dp>0.5:
@@ -87,21 +88,30 @@ else:
     if len(positions) == 0:
         print("No positions found. Cannot sell or execute order.")
     else:
-        # choose random position and determine nominal value
-        random_position = random.choice(positions) 
-        market_value = float(random_position["market_value"])
+        # ensure the chosen position can be sold (i.e. market value > 0.01)
+        for random_position in random.sample(positions, len(positions)):
+            market_value = float(random_position["market_value"])
+            if market_value > 0.01:
+                # Found a sellable position.
+                print("Sellable position found:", random_position)
+                break
+        else:
+            # exit script if no valid positions
+            print("No valid positions to sell.")
+            sys.exit()
 
         # set payload symbol
         payload["symbol"] = random_position["symbol"]
 
         # either set payload value as meal amount or nominal value owned
         if (mcdonald_meal[0]>market_value):
-            payload["notional"] = market_value 
+            payload["notional"] = round(market_value,2)
         else:
             payload["notional"] = mcdonald_meal[0]
         
         # place order
         print("Selling", payload["notional"], "of", random_position["symbol"])
         response = requests.post("https://paper-api.alpaca.markets/v2/orders", json=payload, headers=headers)
+        print(response.text)
 
 
